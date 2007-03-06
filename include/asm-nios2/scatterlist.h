447375b38bd7327e47b0601c16c323eabcc5f23d
Jean Delvare <khali@linux-fr.org>
2007-03-06 02:45:12 -0800

    PCI: scatterlist.h needs types.h
    Most architectures' scatterlist.h use the type dma_addr_t, but omit to
    include <asm/types.h> which defines it.  This could lead to build failures,
    so let's add the missing includes.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/asm-nios2/scatterlist.h
+++ include/asm-nios2/scatterlist.h
@@ -1,6 +1,8 @@
 #ifndef _ASM_NIOS2_SCATTERLIST_H
 #define _ASM_NIOS2_SCATTERLIST_H
 
+#include <asm/types.h>
+
 struct scatterlist {
     struct page * page; /* Location for highmem page, if any */
     unsigned int offset;/* for highmem, page offset */
