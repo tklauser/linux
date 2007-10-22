974b5c6dd7142610cf36c72add9cea5eb591c74d
Jens Axboe <jens.axboe@oracle.com>
2007-10-22 19:57:20 +0200

    Change table chaining layout
    Change the page member of the scatterlist structure to be an unsigned
    long, and encode more stuff in the lower bits:
    
    - Bits 0 and 1 zero: this is a normal sg entry. Next sg entry is located
      at sg + 1.
    - Bit 0 set: this is a chain entry, the next real entry is at ->page_link
      with the two low bits masked off.
    - Bit 1 set: this is the final entry in the sg entry. sg_next() will return
      NULL when passed such an entry.
    
    It's thus important that sg table users use the proper accessors to get
    and set the page member.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- include/asm-nios2/scatterlist.h
+++ include/asm-nios2/scatterlist.h
@@ -4,10 +4,10 @@
 #include <asm/types.h>
 
 struct scatterlist {
-    struct page * page; /* Location for highmem page, if any */
-    unsigned int offset;/* for highmem, page offset */
-    dma_addr_t dma_address;
-    unsigned int length;
+	unsigned long	page_link;
+	unsigned int offset;/* for highmem, page offset */
+	dma_addr_t dma_address;
+	unsigned int length;
 };
 
 #define ISA_DMA_THRESHOLD (0xffffffff)
