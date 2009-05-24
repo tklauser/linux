98c8609bffca9cb33cc1487b816210d43cdc8801
Thomas Chou <thomas@wytron.com.tw>
2009-05-24 10:35:45 +0800

    remove unused TIF_NOTIFY_RESUME flag
    Remove unused TIF_NOTIFY_RESUME flag for all processor architectures.  The
    flag was not used excecpt on IA-64 where the patch replaces it with
    TIF_PERFMON_WORK.
    
    Signed-off-by: stephane eranian <eranian@hpl.hp.com>
    Cc: <linux-arch@vger.kernel.org>
    Cc: "Luck, Tony" <tony.luck@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- include/asm-nios2/thread_info.h
+++ include/asm-nios2/thread_info.h
@@ -123,7 +123,6 @@ static inline struct thread_info *current_thread_info(void)
  * - pending work-to-be-done flags are in LSW
  * - other flags in MSW
  */
-#define TIF_NOTIFY_RESUME	1	/* resumption notification requested */
 #define TIF_SIGPENDING		2	/* signal pending */
 #define TIF_NEED_RESCHED	3	/* rescheduling necessary */
 #define TIF_SYSCALL_AUDIT	4	/* syscall auditing active */
@@ -136,7 +135,6 @@ static inline struct thread_info *current_thread_info(void)
 #define TIF_SYSCALL_TRACE	31	/* syscall trace active */
 
 #define _TIF_SYSCALL_TRACE	(1<<TIF_SYSCALL_TRACE)
-#define _TIF_NOTIFY_RESUME	(1<<TIF_NOTIFY_RESUME)
 #define _TIF_SIGPENDING		(1<<TIF_SIGPENDING)
 #define _TIF_NEED_RESCHED	(1<<TIF_NEED_RESCHED)
 #define _TIF_SYSCALL_AUDIT	(1<<TIF_SYSCALL_AUDIT)
