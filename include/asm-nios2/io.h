73923b94d8b696abe11e90dfc98106fa146ef587
Thomas Chou <thomas@wytron.com.tw>
2009-05-24 11:08:44 +0800

    nios2: ioremap should be nocache
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- include/asm-nios2/io.h
+++ include/asm-nios2/io.h
@@ -1,25 +1,3 @@
-/*
- * Copyright (C) 2004, Microtronix Datacom Ltd.
- *
- * All rights reserved.          
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-
 #ifndef _ASM_NIOS2_IO_H
 #define _ASM_NIOS2_IO_H
 
@@ -205,7 +183,7 @@ extern void __iounmap(void *addr);
 
 extern inline void *ioremap(unsigned long physaddr, unsigned long size)
 {
-	return __ioremap(physaddr, size, _PAGE_CACHED);
+	return __ioremap(physaddr, size, 0);
 }
 extern inline void *ioremap_nocache(unsigned long physaddr, unsigned long size)
 {
