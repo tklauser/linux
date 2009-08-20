b936aef338156a3b859a8a14a2d74a93348a1366
Thomas Chou <thomas@wytron.com.tw>
2009-08-20 16:51:29 +0800

    nios2: fix zImage jump start address
    The previous jump start address to boot linux was wrong.
    Revert to run uncompressor in cached kernel space.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/boot/compressed/misc.c
+++ arch/nios2/boot/compressed/misc.c
@@ -158,7 +158,7 @@ static void error(char *x)
 
 void decompress_kernel(void)
 {
-  output_data = (void *)(DDR2_TOP_BASE | IO_REGION_BASE);
+  output_data = (void *)(DDR2_TOP_BASE | KERNEL_REGION_BASE);
   output_ptr = 0;
   free_mem_ptr = (unsigned long)&_end;
   free_mem_end_ptr = free_mem_ptr + HEAP_SIZE;
@@ -172,7 +172,7 @@ void decompress_kernel(void)
   puts("start at ");
   putx(DDR2_TOP_BASE | KERNEL_REGION_BASE);
   putchar('\n');
-  dump(DDR2_TOP_BASE | IO_REGION_BASE, 16);
+  dump(DDR2_TOP_BASE | KERNEL_REGION_BASE, 64);
   putchar('\n');
 #endif
 }
