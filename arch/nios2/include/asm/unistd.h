f604e3aec6e4a5c05ce3b69f172957d92a605ac5
Tobias Klauser <klto@zhaw.ch>
2009-09-24 08:26:10 +0200

    nios2: Wire up perf_counter_open syscall
    Acked-by: Thomas Chou <thomas@wytron.com.tw>
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/unistd.h
+++ arch/nios2/include/asm/unistd.h
@@ -377,8 +377,9 @@ extern int unresolved_symbol;
 #define __NR_preadv		331
 #define __NR_pwritev		332
 #define __NR_rt_tgsigqueueinfo	333
+#define __NR_perf_counter_open	334
 
-#define NR_syscalls                     334
+#define NR_syscalls                     335
 
 #ifdef __KERNEL__
 
