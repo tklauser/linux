4f7d205b6f6e64dd7c28e2878eb633947fadf903
Tobias Klauser <klto@zhaw.ch>
2009-10-02 07:36:18 +0200

    altspi: Readd license information to the file header
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- drivers/spi/altspi.c
+++ drivers/spi/altspi.c
@@ -2,7 +2,12 @@
  * Altera SPI driver
  *
  * Based on spi_s3c24xx.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  */
+
 #include <linux/init.h>
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
