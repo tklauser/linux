70a8982782e98d790ab51e790e248dbfe51afad9
Tobias Klauser <klto@zhaw.ch>
2009-12-14 13:45:55 +0100

    nios2: Remove duplicate do_mmap2 from arch/nios2/kernel/syscall.c
    The function is not used inside syscall.c and the exact same function
    already exists in arch/nios2/kernel/sys_nios2.c and is used there.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/kernel/syscall.c
+++ arch/nios2/kernel/syscall.c
@@ -94,31 +94,6 @@ unsigned long arch_get_unmapped_area(struct file *filp, unsigned long addr,
 	}
 }
 
-/* common code for old and new mmaps */
-static inline unsigned long
-do_mmap2(unsigned long addr, unsigned long len, unsigned long prot,
-        unsigned long flags, unsigned long fd, unsigned long pgoff)
-{
-	unsigned long error = -EBADF;
-	struct file * file = NULL;
-
-	flags &= ~(MAP_EXECUTABLE | MAP_DENYWRITE);
-	if (!(flags & MAP_ANONYMOUS)) {
-		file = fget(fd);
-		if (!file)
-			goto out;
-	}
-
-	down_write(&current->mm->mmap_sem);
-	error = do_mmap_pgoff(file, addr, len, prot, flags, pgoff);
-	up_write(&current->mm->mmap_sem);
-
-	if (file)
-		fput(file);
-out:
-	return error;
-}
-
 /*
  * Compacrapability ...
  */
