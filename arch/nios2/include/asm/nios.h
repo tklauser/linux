76947530715116d344d6b8de22a635c26b96199e
Thomas Chou <thomas@wytron.com.tw>
2010-03-21 11:02:34 +0800

    nios2: move compability macro to fpga def in nios.h
    It will be cleaner with compability macro coming with fpga def.
    Each fpga design should have its own entry.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/include/asm/nios.h
+++ arch/nios2/include/asm/nios.h
@@ -3,10 +3,45 @@
 
 #if defined(CONFIG_NIOS2_DEFAULT_MMU)
 #include <asm/default_mmu.h>
+
+/* Added compability mode with macro names from "old" design...
+ * FIXME: we should really fix drivers instead. but this makes it
+ * easier to switch between old and new design...
+ */
+#define CONFIG_ALTERA_CYCLONE_III
+#define DDR2_TOP_BASE DDR2_LO_LATENCY_128M_BASE
+#define DDR2_TOP_SPAN DDR2_LO_LATENCY_128M_SPAN
+#define EXT_FLASH_BASE CFI_FLASH_64M_BASE
+#define EXT_FLASH_SPAN CFI_FLASH_64M_SPAN
+#define TIMER_1MS_FREQ LINUX_TIMER_1MS_FREQ
+#define TIMER_1MS_BASE LINUX_TIMER_1MS_BASE
+#define TIMER_1MS_SPAN LINUX_TIMER_1MS_SPAN
+#define TIMER_1MS_IRQ LINUX_TIMER_1MS_IRQ
+
 #elif defined(CONFIG_NIOS2_MAXIMUM_MMU)
 #include <asm/maximum_mmu.h>
+
+#define CONFIG_ALTERA_CYCLONE_III
+#define DDR2_TOP_BASE DDR2_LO_LATENCY_128M_BASE
+#define DDR2_TOP_SPAN DDR2_LO_LATENCY_128M_SPAN
+#define EXT_FLASH_BASE CFI_FLASH_64M_BASE
+#define EXT_FLASH_SPAN CFI_FLASH_64M_SPAN
+#define TIMER_1MS_FREQ LINUX_TIMER_1MS_FREQ
+#define TIMER_1MS_BASE LINUX_TIMER_1MS_BASE
+#define TIMER_1MS_SPAN LINUX_TIMER_1MS_SPAN
+#define TIMER_1MS_IRQ LINUX_TIMER_1MS_IRQ
+
 #elif defined(CONFIG_NIOS2_CUSTOM_FPGA)
 #include <asm/custom_fpga.h>
+
+#define CONFIG_ALTERA_NEEK_C3
+#define DDR2_TOP_BASE DDR_SDRAM_BASE
+#define DDR2_TOP_SPAN DDR_SDRAM_SPAN
+#define TIMER_1MS_FREQ SYS_CLK_TIMER_FREQ
+#define TIMER_1MS_BASE SYS_CLK_TIMER_BASE
+#define TIMER_1MS_SPAN SYS_CLK_TIMER_SPAN
+#define TIMER_1MS_IRQ SYS_CLK_TIMER_IRQ
+
 #else
 #error "No FPGA configuration selected
 #endif
@@ -14,64 +49,6 @@
 /* Fake CPU frequency if not defined by the design header file */
 #ifndef CPU_FREQ
 # define CPU_FREQ	0
-#endif
-
-/* Added compability mode with macro names from "old" design...
- * FIXME: we should really fix drivers instead. but this makes it
- * easier to switch between old and new design...
- */
-
-#ifdef LINUX_TIMER_1MS_FREQ
-
-#define CONFIG_ALTERA_CYCLONE_III
-#define DDR2_TOP_BASE DDR2_LO_LATENCY_128M_BASE
-#define DDR2_TOP_SPAN DDR2_LO_LATENCY_128M_SPAN
-
-#define EXT_FLASH_BASE CFI_FLASH_64M_BASE
-#define EXT_FLASH_SPAN CFI_FLASH_64M_SPAN
-
-#define TIMER_1MS_FREQ LINUX_TIMER_1MS_FREQ
-#define TIMER_1MS_BASE LINUX_TIMER_1MS_BASE
-#define TIMER_1MS_SPAN LINUX_TIMER_1MS_SPAN
-#define TIMER_1MS_IRQ LINUX_TIMER_1MS_IRQ
-
-#endif
-
-/* 2C35 dev board */
-#ifdef TIMER_0_FREQ
-
-#define CONFIG_ALTERA_CYCLONE_II
-#define DDR2_TOP_BASE SDRAM_0_BASE
-#define DDR2_TOP_SPAN SDRAM_0_SPAN
-
-#define TIMER_1MS_FREQ TIMER_0_FREQ
-#define TIMER_1MS_BASE TIMER_0_BASE
-#define TIMER_1MS_SPAN TIMER_0_SPAN
-#define TIMER_1MS_IRQ TIMER_0_IRQ
-
-#define UART_BASE UART0_BASE
-#define UART_SPAN UART0_SPAN
-#define UART_IRQ UART0_IRQ
-#define UART_FREQ UART0_FREQ
-
-#define JTAG_UART_BASE JTAG_UART_0_BASE
-#define JTAG_UART_SPAN JTAG_UART_0_SPAN
-#define JTAG_UART_IRQ JTAG_UART_0_IRQ
-
-#endif
-
-/* NEEK dev board */
-#ifdef SYS_CLK_TIMER_BASE
-
-#define CONFIG_ALTERA_NEEK_C3
-#define DDR2_TOP_BASE DDR_SDRAM_BASE
-#define DDR2_TOP_SPAN DDR_SDRAM_SPAN
-
-#define TIMER_1MS_FREQ SYS_CLK_TIMER_FREQ
-#define TIMER_1MS_BASE SYS_CLK_TIMER_BASE
-#define TIMER_1MS_SPAN SYS_CLK_TIMER_SPAN
-#define TIMER_1MS_IRQ SYS_CLK_TIMER_IRQ
-
 #endif
 
 /* Nios II Constants */
