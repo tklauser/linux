6bb7fa9f8b245c7a247af7a947bf00b7308dc52a
Tobias Klauser <klto@zhaw.ch>
2010-06-01 14:42:00 +0200

    nios2: Adjust cpuinfo for MMU/NOMMU, clean up coding style
    Add some more information to cpuinfo:
     - MUL, MULX and DIV instructions supported
     - Icache and Dcache size and line length
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/kernel/setup.c
+++ arch/nios2/kernel/setup.c
@@ -1,40 +1,15 @@
 /*
-  21Mar2001    1.1    dgt/microtronix: Altera Excalibur/Nios32 port
-  30Jun2003           kenw/microtronix: Remove cmdline check in flash
-*/
-
-/*
- *  linux/arch/niosnommu/kernel/setup.c
+ * Nios2-specific parts of system setup
  *
- *  Copyright (C) 2004       Microtronix Datacom Ltd.
- *  Copyright (C) 2001       Vic Phillips {vic@microtronix.com}
- *  Copyleft  (C) 2000       James D. Schettine {james@telos-systems.com}
- *  Copyright (C) 1999       Greg Ungerer (gerg@moreton.com.au)
- *  Copyright (C) 1998,2000  D. Jeff Dionne <jeff@lineo.ca>
- *  Copyright (C) 1998       Kenneth Albanowski <kjahds@kjahds.com>
- *  Copyright (C) 1995       Hamish Macdonald
+ * Copyright (C) 2010 Tobias Klauser <tklauser@distanz.ch>
+ * Copyright (C) 2004 Microtronix Datacom Ltd.
+ * Copyright (C) 2001 Vic Phillips <vic@microtronix.com>
  *
- * All rights reserved.          
+ * based on kernel/setup.c from m68knommu
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-
-/*
- * This file handles the architecture-dependent parts of system setup
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file "COPYING" in the main directory of this archive
+ * for more details.
  */
 
 #include <linux/kernel.h>
@@ -65,37 +40,38 @@
 #include <asm/pgtable.h>
 #include <asm/setup.h>
 
-/* Sanity check config options for HW supported mul, mulx, div against
+/*
+ * Sanity check config options for HW supported mul, mulx, div against
  * the generated header for the specified design.
  */
 #if defined(CONFIG_NIOS2_HW_MUL_SUPPORT)
-#if HARDWARE_MULTIPLY_PRESENT == 0
-#error Kernel compiled with mul support although not enabled in design
-#endif
+# if HARDWARE_MULTIPLY_PRESENT == 0
+#  error Kernel compiled with MUL support although not enabled in design
+# endif
 #else
-#if HARDWARE_MULTIPLY_PRESENT != 0
-#warning Kernel compiled without mul support although enabled in design
-#endif
+# if HARDWARE_MULTIPLY_PRESENT != 0
+#  warning Kernel compiled without MUL support although enabled in design
+# endif
 #endif
 
 #if defined(CONFIG_NIOS2_HW_MULX_SUPPORT)
-#if HARDWARE_MULX_PRESENT == 0
-#error Kernel compiled with mulx support although not enabled in design
-#endif
+# if HARDWARE_MULX_PRESENT == 0
+#  error Kernel compiled with MULX support although not enabled in design
+# endif
 #else
-#if HARDWARE_MULX_PRESENT != 0
-#warning Kernel compiled without mulx support although enabled in design
-#endif
+# if HARDWARE_MULX_PRESENT != 0
+#  warning Kernel compiled without MULX support although enabled in design
+# endif
 #endif
 
 #if defined(CONFIG_NIOS2_HW_DIV_SUPPORT)
-#if HARDWARE_DIVIDE_PRESENT == 0
-#error Kernel compiled with div support although not enabled in design
-#endif
+# if HARDWARE_DIVIDE_PRESENT == 0
+#  error Kernel compiled with DIV support although not enabled in design
+# endif
 #else
-#if HARDWARE_DIVIDE_PRESENT != 0
-#warning Kernel compiled without div support although enabled in design
-#endif
+# if HARDWARE_DIVIDE_PRESENT != 0
+#  warning Kernel compiled without DIV support although enabled in design
+# endif
 #endif
 
 #ifdef CONFIG_CONSOLE
@@ -117,10 +93,8 @@ static char __initdata command_line[COMMAND_LINE_SIZE] = { 0, };
 
 /*				   r1  r2  r3  r4  r5  r6  r7  r8  r9 r10 r11*/
 /*				   r12 r13 r14 r15 or2                      ra  fp  sp  gp es  ste  ea*/
-static struct pt_regs fake_regs = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,\
+static struct pt_regs fake_regs = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 				    0,  0,  0,  0,  0, (unsigned long)cpu_idle,  0,  0,  0, 0, 0};
-
-#define CPU "NIOS2"
 
 extern void __init mmu_init(void);
 
@@ -161,14 +135,13 @@ void __init setup_arch(char **cmdline_p)
 #endif
 
 	memory_start = PAGE_ALIGN((unsigned long)__pa(&_end));
-	memory_end = (unsigned long) DDR2_TOP_BASE + DDR2_TOP_SPAN;	
+	memory_end = (unsigned long) DDR2_TOP_BASE + DDR2_TOP_SPAN;
 
 #ifndef CONFIG_PASS_CMDLINE
 	memcpy(command_line, default_command_line, sizeof(default_command_line));
 #endif
 
-	printk("\r\n\nLinux/Nios II-MMU\n");
-
+	printk("%s/Nios II\n", UTS_SYSNAME);
 
 	init_mm.start_code = (unsigned long) &_stext;
 	init_mm.end_code = (unsigned long) &_etext;
@@ -186,19 +159,19 @@ void __init setup_arch(char **cmdline_p)
 	 * give all the memory to the bootmap allocator,  tell it to put the
 	 * boot mem_map at the start of memory
 	 */
-	printk("init_bootmem_node(?,%#lx, %#x, %#lx)\n", 
+	printk("init_bootmem_node(?,%#lx, %#x, %#lx)\n",
 	       PFN_UP(memory_start), PFN_DOWN(PHYS_OFFSET), PFN_DOWN(memory_end));
 	bootmap_size = init_bootmem_node(NODE_DATA(0),
 					 PFN_UP(memory_start),
 					 PFN_DOWN(PHYS_OFFSET),
 					 PFN_DOWN(memory_end));
-	
+
 	/*
 	 * free the usable memory,  we have to make sure we do not free
 	 * the bootmem bitmap so we then reserve it after freeing it :-)
 	 */
-	printk("free_bootmem(%#lx, %#lx)\n", 
-          memory_start, memory_end - memory_start);
+	printk("free_bootmem(%#lx, %#lx)\n",
+	       memory_start, memory_end - memory_start);
 	free_bootmem(memory_start, memory_end - memory_start);
 
         /*
@@ -213,9 +186,8 @@ void __init setup_arch(char **cmdline_p)
         reserve_bootmem(memory_start, bootmap_size, BOOTMEM_DEFAULT);
 
 #ifdef CONFIG_BLK_DEV_INITRD
-	if (initrd_start) {
-	  reserve_bootmem(virt_to_phys((void *)initrd_start), initrd_end - initrd_start, BOOTMEM_DEFAULT);
-	}
+	if (initrd_start)
+		reserve_bootmem(virt_to_phys((void *)initrd_start), initrd_end - initrd_start, BOOTMEM_DEFAULT);
 #endif /* CONFIG_BLK_DEV_INITRD */
 
 	/*
@@ -229,46 +201,77 @@ void __init setup_arch(char **cmdline_p)
 #endif
 }
 
+void arch_gettod(int *year, int *month, int *date, int *hour, int *min, int *sec)
+{
+	*year = *month = *date = *hour = *min = *sec = 0;
+}
+
+#ifdef CONFIG_PROC_FS
+
 /*
  *	Get CPU information for use by the procfs.
  */
 static int show_cpuinfo(struct seq_file *m, void *v)
 {
-	const char *cpu, *cpu_impl, *fpu;
+	int count = 0;
+	const char *cpu_impl = CPU_IMPLEMENTATION;
 	const u_long clockfreq = CPU_FREQ;
 
-	cpu = CPU;
-	cpu_impl = CPU_IMPLEMENTATION;
-	fpu = "none";
+	count = seq_printf(m,
+			"CPU:\t\tNios II/%s\n"
+#if defined(MMU_PRESENT) && defined(CONFIG_MMU)
+			"MMU:\t\t%u-way TLB, %d entries\n"
+#else
+			"MMU:\t\tnone\n"
+#endif
+			"FPU:\t\tnone\n"
+			"Clocking:\t%lu.%02lu MHz\n"
+			"BogoMips:\t%lu.%02lu\n"
+			"Calibration:\t%lu loops\n",
+			cpu_impl,
+#if defined(MMU_PRESENT) && defined(CONFIG_MMU)
+			TLB_NUM_WAYS, TLB_NUM_ENTRIES,
+#endif
+			clockfreq / 1000000, (clockfreq / 100000) % 10,
+			(loops_per_jiffy * HZ) / 500000, ((loops_per_jiffy * HZ) / 5000) % 100,
+			(loops_per_jiffy * HZ));
 
-	seq_printf(m, "CPU:\t\t%s/%s\n"
-		      "MMU:\t\tways:%d entries:%d\n"
-		      "FPU:\t\t%s\n"
-		      "Clocking:\t%lu.%02lu MHz\n"
-		      "BogoMips:\t%lu.%02lu\n"
-		      "Calibration:\t%lu loops\n",
-		      cpu, cpu_impl,
-		      TLB_NUM_WAYS, TLB_NUM_ENTRIES,
-		      fpu,
-		      clockfreq / 1000000, (clockfreq / 100000) % 10,
-		      (loops_per_jiffy * HZ) / 500000, ((loops_per_jiffy * HZ) / 5000) % 100,
-		      (loops_per_jiffy * HZ));
+	count += seq_printf(m,
+			"HW:\n"
+			" MUL:\t\t%s\n"
+			" MULX:\t\t%s\n"
+			" DIV:\t\t%s\n",
+			HARDWARE_MULTIPLY_PRESENT ? "yes" : "no",
+			HARDWARE_MULX_PRESENT ? "yes" : "no",
+			HARDWARE_DIVIDE_PRESENT ? "yes" : "no");
+
+	count += seq_printf(m,
+			"Icache:\t\t%ukB, line length: %u\n",
+			ICACHE_SIZE >> 10,
+			ICACHE_LINE_SIZE);
+
+	count += seq_printf(m,
+			"Dcache:\t\t%ukB, line length: %u\n",
+			DCACHE_SIZE >> 10,
+			DCACHE_LINE_SIZE);
 
 	return 0;
 }
 
-static void *cpuinfo_start (struct seq_file *m, loff_t *pos)
+static void *cpuinfo_start(struct seq_file *m, loff_t *pos)
 {
-	return *pos < NR_CPUS ? ((void *) 0x12345678) : NULL;
+	unsigned long i = *pos;
+
+	return i < NR_CPUS ? (void *) (i + 1) : NULL;
 }
 
-static void *cpuinfo_next (struct seq_file *m, void *v, loff_t *pos)
+static void *cpuinfo_next(struct seq_file *m, void *v, loff_t *pos)
 {
 	++*pos;
-	return cpuinfo_start (m, pos);
+	return cpuinfo_start(m, pos);
 }
 
-static void cpuinfo_stop (struct seq_file *m, void *v)
+static void cpuinfo_stop(struct seq_file *m, void *v)
 {
 }
 
@@ -278,3 +281,5 @@ const struct seq_operations cpuinfo_op = {
 	.stop	= cpuinfo_stop,
 	.show	= show_cpuinfo
 };
+
+#endif /* CONFIG_PROC_FS */
