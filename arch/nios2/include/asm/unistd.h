082cc415c8f0cf34facc5a17a7fab8bcc063f0bf
Frans Meulenbroeks <fransmeulenbroeks@gmail.com>
2010-06-18 14:15:17 +0200

    nios2/include/asm/unistd.h: comment out entries for system calls that are not implemented
    Comment out entries for system callss that are not implemented.
    These cause faulty code. E.g. there is no waitpid system call but if __NR__waitpid is
    there glibc will try to invoke this system call.
    Commenting out those entries cures this.
    
    Signed-off-by: Frans Meulenbroeks <fransmeulenbroeks@gmail.com>

--- arch/nios2/include/asm/unistd.h
+++ arch/nios2/include/asm/unistd.h
@@ -21,11 +21,12 @@
  *
  * This file was copied from nios2 and reworked.
  *
- * Dec/12/2008    frma/ivho     Ported to nios2 with mmu.
+ * Dec/12/2008    frma/ivho     	Ported to nios2 with mmu.
+ * Jun/18/2010    Frans Meulenbroeks	Commented out all not implemented calls
  *
  ---------------------------------------------------------------------*/
 
-#if defined(__ASSEMBLY__) || defined(ASSEMBLER)
+#if defined(__ASSEMBLY__) || defined(ASSEMBLER) || defined(__ASSEMBLER__)
 
 #define UR ERROR
 
@@ -47,7 +48,7 @@ extern int unresolved_symbol;
 #define __NR_write		  4
 #define __NR_open		  5
 #define __NR_close		  6
-#define __NR_waitpid		  7
+/* #define __NR_waitpid		  7 */
 #define __NR_creat		  8
 #define __NR_link		  9
 #define __NR_unlink		 10
@@ -57,8 +58,8 @@ extern int unresolved_symbol;
 #define __NR_mknod		 14
 #define __NR_chmod		 15
 #define __NR_chown		 16
-#define __NR_break		 17
-#define __NR_oldstat		 18
+/* #define __NR_break		 17 */
+/* #define __NR_oldstat		 18 */
 #define __NR_lseek		 19
 #define __NR_getpid		 20
 #define __NR_mount		 21
@@ -68,14 +69,14 @@ extern int unresolved_symbol;
 #define __NR_stime		 25
 #define __NR_ptrace		 26
 #define __NR_alarm		 27
-#define __NR_oldfstat		 28
+/* #define __NR_oldfstat		 28 */
 #define __NR_pause		 29
 #define __NR_utime		 30
-#define __NR_stty		 31
-#define __NR_gtty		 32
+/* #define __NR_stty		 31 */
+/* #define __NR_gtty		 32 */
 #define __NR_access		 33
 #define __NR_nice		 34
-#define __NR_ftime		 35
+/* #define __NR_ftime		 35 */
 #define __NR_sync		 36
 #define __NR_kill		 37
 #define __NR_rename		 38
@@ -84,22 +85,22 @@ extern int unresolved_symbol;
 #define __NR_dup		 41
 #define __NR_pipe		 42
 #define __NR_times		 43
-#define __NR_prof		 44
+/* #define __NR_prof		 44 */
 #define __NR_brk		 45
 #define __NR_setgid		 46
 #define __NR_getgid		 47
-#define __NR_signal		 48
+/* #define __NR_signal		 48 */
 #define __NR_geteuid		 49
 #define __NR_getegid		 50
 #define __NR_acct		 51
 #define __NR_umount2		 52
-#define __NR_lock		 53
+/* #define __NR_lock		 53 */
 #define __NR_ioctl		 54
 #define __NR_fcntl		 55
-#define __NR_mpx		 56
+/* #define __NR_mpx		 56 */
 #define __NR_setpgid		 57
-#define __NR_ulimit		 58
-#define __NR_oldolduname	 59
+/* #define __NR_ulimit		 58 */
+/* #define __NR_oldolduname	 59 */
 #define __NR_umask		 60
 #define __NR_chroot		 61
 #define __NR_ustat		 62
@@ -108,15 +109,15 @@ extern int unresolved_symbol;
 #define __NR_getpgrp		 65
 #define __NR_setsid		 66
 #define __NR_sigaction		 67
-#define __NR_sgetmask		 68
-#define __NR_ssetmask		 69
+/* #define __NR_sgetmask		 68 */
+/* #define __NR_ssetmask		 69 */
 #define __NR_setreuid		 70
 #define __NR_setregid		 71
 #define __NR_sigsuspend		 72
 #define __NR_sigpending		 73
 #define __NR_sethostname	 74
 #define __NR_setrlimit		 75
-#define __NR_getrlimit		 76
+/* #define __NR_getrlimit		 76 */
 #define __NR_getrusage		 77
 #define __NR_gettimeofday	 78
 #define __NR_settimeofday	 79
@@ -124,12 +125,12 @@ extern int unresolved_symbol;
 #define __NR_setgroups		 81
 #define __NR_select		 82
 #define __NR_symlink		 83
-#define __NR_oldlstat		 84
+/* #define __NR_oldlstat		 84 */
 #define __NR_readlink		 85
 #define __NR_uselib		 86
 #define __NR_swapon		 87
 #define __NR_reboot		 88
-#define __NR_readdir		 89
+/* #define __NR_readdir		 89 */
 #define __NR_mmap		 90
 #define __NR_munmap		 91
 #define __NR_truncate		 92
@@ -138,10 +139,10 @@ extern int unresolved_symbol;
 #define __NR_fchown		 95
 #define __NR_getpriority	 96
 #define __NR_setpriority	 97
-#define __NR_profil		 98
+/* #define __NR_profil		 98 */
 #define __NR_statfs		 99
 #define __NR_fstatfs		100
-#define __NR_ioperm		101
+/* #define __NR_ioperm		101 */
 #define __NR_socketcall		102
 #define __NR_syslog		103
 #define __NR_setitimer		104
@@ -149,10 +150,10 @@ extern int unresolved_symbol;
 #define __NR_stat		106
 #define __NR_lstat		107
 #define __NR_fstat		108
-#define __NR_olduname		109
-#define __NR_iopl		/* 110 */ not supported
+/* #define __NR_olduname		109 */
+/* #define __NR_iopl		110 */
 #define __NR_vhangup		111
-#define __NR_idle		/* 112 */ Obsolete
+/* #define __NR_idle		112 */
 #define __NR_nios2cmpxchg	113
 #define __NR_wait4		114
 #define __NR_swapoff		115
@@ -167,17 +168,17 @@ extern int unresolved_symbol;
 #define __NR_adjtimex		124
 #define __NR_mprotect		125
 #define __NR_sigprocmask	126
-#define __NR_create_module	127
+/* #define __NR_create_module	127 */
 #define __NR_init_module	128
 #define __NR_delete_module	129
-#define __NR_get_kernel_syms	130
+/* #define __NR_get_kernel_syms	130 */
 #define __NR_quotactl		131
 #define __NR_getpgid		132
 #define __NR_fchdir		133
 #define __NR_bdflush		134
 #define __NR_sysfs		135
 #define __NR_personality	136
-#define __NR_afs_syscall	137 /* Syscall for Andrew File System */
+/* #define __NR_afs_syscall	137 Syscall for Andrew File System */
 #define __NR_setfsuid		138
 #define __NR_setfsgid		139
 #define __NR__llseek		140
@@ -207,7 +208,7 @@ extern int unresolved_symbol;
 #define __NR_setresuid		164
 #define __NR_getresuid		165
 #define __NR_getpagesize	166
-#define __NR_query_module	167
+/* #define __NR_query_module	167 */
 #define __NR_poll		168
 #define __NR_nfsservctl		169
 #define __NR_setresgid		170
@@ -220,16 +221,16 @@ extern int unresolved_symbol;
 #define __NR_rt_sigtimedwait	177
 #define __NR_rt_sigqueueinfo	178
 #define __NR_rt_sigsuspend	179
-#define __NR_pread		180
-#define __NR_pwrite		181
+/* #define __NR_pread		180	we cannot have both NR_pread and NR_pread64   */
+/* #define __NR_pwrite		181	we cannot have both NR_pwrite and NR_pwrite64 */
 #define __NR_lchown		182
 #define __NR_getcwd		183
 #define __NR_capget		184
 #define __NR_capset		185
 #define __NR_sigaltstack	186
 #define __NR_sendfile		187
-#define __NR_getpmsg		188	/* some people actually want streams */
-#define __NR_putpmsg		189	/* some people actually want streams */
+/* #define __NR_getpmsg		188	some people actually want streams */
+/* #define __NR_putpmsg		189	some people actually want streams */
 #define __NR_vfork		190
 #define __NR_ugetrlimit		191
 #define __NR_mmap2		192
@@ -318,10 +319,10 @@ extern int unresolved_symbol;
 #define __NR_mq_notify		275
 #define __NR_mq_getsetattr	276
 #define __NR_waitid		277
-#define __NR_sys_setaltroot	278
-#define __NR_add_key		279
-#define __NR_request_key	280
-#define __NR_keyctl		281
+/* #define __NR_sys_setaltroot	278 */
+/* #define __NR_add_key		279 */
+/* #define __NR_request_key	280 */
+/* #define __NR_keyctl		281 */
 #define __NR_ioprio_set		282
 #define __NR_ioprio_get		283
 #define __NR_inotify_init	284
@@ -391,7 +392,7 @@ extern int unresolved_symbol;
 #define __ARCH_WANT_SYS_SIGNAL
 #define __ARCH_WANT_SYS_TIME
 #define __ARCH_WANT_SYS_UTIME
-#define __ARCH_WANT_SYS_WAITPID
+/* #define __ARCH_WANT_SYS_WAITPID */
 #define __ARCH_WANT_SYS_SOCKETCALL
 #define __ARCH_WANT_SYS_FADVISE64
 #define __ARCH_WANT_SYS_GETPGRP
