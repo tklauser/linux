003283343e17f035b48d6a74b68840f65941daea
Tobias Klauser <klto@zhaw.ch>
2010-09-23 10:08:12 +0200

    nios2: Make asm/ucontext.h ready for CONFIG_MMU
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/ucontext.h
+++ arch/nios2/include/asm/ucontext.h
@@ -1,29 +1,16 @@
+/*
+ * Copyright (C) 2010 Tobias Klauser <tklauser@distanz.ch>
+ * Copyright (C) 2004 Microtronix Datacom Ltd
+ *
+ * derived from m68knommu
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
+
 #ifndef _ASM_NIOS2_UCONTEXT_H
 #define _ASM_NIOS2_UCONTEXT_H
-
-/*--------------------------------------------------------------------
- *
- * include/asm-nios2/ucontext.h
- *
- * Derived from M68knommu
- *
- * Copyright (C) 2004   Microtronix Datacom Ltd
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- *
- * Jan/20/2004		dgt	    NiosII
- *
- ---------------------------------------------------------------------*/
-
 
 typedef int greg_t;
 #define NGREG 32
@@ -31,6 +18,9 @@ typedef greg_t gregset_t[NGREG];
 
 struct mcontext {
 	int version;
+#ifndef CONFIG_MMU
+	int status_extension;
+#endif
 	gregset_t gregs;
 };
 
