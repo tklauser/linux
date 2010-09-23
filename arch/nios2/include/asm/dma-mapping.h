c6676633b333e7efb7c4b1d66b191b3927600c0f
Thomas Chou <thomas@wytron.com.tw>
2010-09-23 10:36:25 +0800

    dma-mapping: unify dma_get_cache_alignment implementations
    Follow commit 4565f0170dfc849b3629c27d769db800467baa62,
    
    dma_get_cache_alignment returns the minimum DMA alignment.  Architectures
    defines it as ARCH_DMA_MINALIGN (formally ARCH_KMALLOC_MINALIGN).  So we
    can unify dma_get_cache_alignment implementations.
    
    Note that some architectures implement dma_get_cache_alignment wrongly.
    dma_get_cache_alignment() should return the minimum DMA alignment.  So
    fully-coherent architectures should return 1.  This patch also fixes this
    issue.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/include/asm/dma-mapping.h
+++ arch/nios2/include/asm/dma-mapping.h
@@ -56,13 +56,6 @@ dma_set_mask(struct device *dev, u64 mask)
 	return 0;
 }
 
-static inline int
-dma_get_cache_alignment(void)
-{
-	/* XXX Largest on any MIPS */
-	return 128;
-}
-
 extern int dma_is_consistent(dma_addr_t dma_addr);
 
 extern void dma_cache_sync(void *vaddr, size_t size,
