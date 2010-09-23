8337fbd8eaaa984538c3f6efc43b0431f9d895d5
Thomas Chou <thomas@wytron.com.tw>
2010-09-23 10:31:28 +0800

    dma-mapping: rename ARCH_KMALLOC_MINALIGN to ARCH_DMA_MINALIGN
    Remove L1_CACHE_ALIGN(x), which is in include/linux/cache.h.
    Add ARCH_DMA_MINALIGN, which might give better kmalloc.
    
    Follow commit a6eb9fe105d5de0053b261148cee56c94b4720ca,
    
    Now each architecture has the own dma_get_cache_alignment implementation.
    
    dma_get_cache_alignment returns the minimum DMA alignment.  Architectures
    define it as ARCH_KMALLOC_MINALIGN (it's used to make sure that malloc'ed
    buffer is DMA-safe; the buffer doesn't share a cache with the others).  So
    we can unify dma_get_cache_alignment implementations.
    
    This patch:
    
    dma_get_cache_alignment() needs to know if an architecture defines
    ARCH_KMALLOC_MINALIGN or not (needs to know if architecture has DMA
    alignment restriction).  However, slab.h define ARCH_KMALLOC_MINALIGN if
    architectures doesn't define it.
    
    Let's rename ARCH_KMALLOC_MINALIGN to ARCH_DMA_MINALIGN.
    ARCH_KMALLOC_MINALIGN is used only in the internals of slab/slob/slub
    (except for crypto).
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/include/asm/cache.h
+++ arch/nios2/include/asm/cache.h
@@ -1,23 +1,7 @@
 /*
  * Copyright (C) 2004 Microtronix Datacom Ltd.
  *
- * All rights reserved.          
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * Licensed under the GPL-2 or later.
  */
 #ifndef _ASM_NIOS2_CACHE_H
 #define _ASM_NIOS2_CACHE_H
@@ -25,10 +9,10 @@
 #include <asm/nios.h>
 
 /* bytes per L1 cache line */
-#define        L1_CACHE_BYTES	ICACHE_LINE_SIZE 	/* 32, this need to be at least 1 */
-#define L1_CACHE_ALIGN(x)	(((x)+(L1_CACHE_BYTES-1))&~(L1_CACHE_BYTES-1))
-#define L1_CACHE_SHIFT	ICACHE_LINE_SIZE_LOG2
+#define L1_CACHE_BYTES		ICACHE_LINE_SIZE
+#define L1_CACHE_SHIFT		ICACHE_LINE_SIZE_LOG2
 
+#define ARCH_DMA_MINALIGN	L1_CACHE_BYTES
 
 #define __cacheline_aligned
 #define ____cacheline_aligned
