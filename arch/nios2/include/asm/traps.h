e890fb37824011a1bdbf838169fc8f7a56a0bcf7
Tobias Klauser <klto@zhaw.ch>
2010-10-06 18:54:41 +0200

    nios2: Add entry.S for NOMMU
    entry.S taken from branch unstable, renamed to entry-nommu.S. The
    -nommu postfix gets added to the filename in the Makefile depending on
    CONFIG_MMU.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

---
+++ arch/nios2/include/asm/traps.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2004 Microtronix Datacom Ltd.
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
+
+#ifndef _ASM_NIOS2_TRAPS_H
+#define _ASM_NIOS2_TRAPS_H
+
+#define TRAP_ID_SYSCALL		0
+#define TRAP_ID_APPDEBUG	1
+
+#endif /* _ASM_NIOS2_TRAPS_H */
