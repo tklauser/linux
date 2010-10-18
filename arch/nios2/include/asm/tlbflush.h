f86806311461abc2608bc55c83ae553324ce9fa2
Tobias Klauser <klto@zhaw.ch>
2010-10-18 15:59:49 +0200

    nios2: Make asm/tlbflush.h ready for CONFIG_MMU
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/tlbflush.h
+++ arch/nios2/include/asm/tlbflush.h
@@ -1,5 +1,17 @@
+/*
+ * Copyright (C) 2010 Tobias Klauser <tklauser@distanz.ch>
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
+
 #ifndef _ASM_NIOS2_TLBFLUSH_H
 #define _ASM_NIOS2_TLBFLUSH_H
+
+#ifndef CONFIG_MMU
+# include <asm-generic/tlbflush.h>
+#else
 
 #include <linux/mm.h>
 
@@ -15,19 +27,20 @@
 extern void local_flush_tlb_all(void);
 extern void local_flush_tlb_mm(struct mm_struct *mm);
 extern void local_flush_tlb_range(struct vm_area_struct *vma,
-	unsigned long start, unsigned long end);
+				  unsigned long start, unsigned long end);
 extern void local_flush_tlb_kernel_range(unsigned long start,
-	unsigned long end);
+					 unsigned long end);
 extern void local_flush_tlb_page(struct vm_area_struct *vma,
 				 unsigned long address);
 extern void local_flush_tlb_one(unsigned long vaddr);
 
-#define flush_tlb_all()			local_flush_tlb_all()
-#define flush_tlb_mm(mm)		local_flush_tlb_mm(mm)
-#define flush_tlb_range(vma,vmaddr,end)	local_flush_tlb_range(vma, vmaddr, end)
-#define flush_tlb_kernel_range(vmaddr,end) \
-	local_flush_tlb_kernel_range(vmaddr, end)
-#define flush_tlb_page(vma,page)	local_flush_tlb_page(vma, page)
-#define flush_tlb_one(vaddr)		local_flush_tlb_one(vaddr)
+#define flush_tlb_all		local_flush_tlb_all
+#define flush_tlb_mm		local_flush_tlb_mm
+#define flush_tlb_range		local_flush_tlb_range
+#define flush_tlb_kernel_range	local_flush_tlb_kernel_range
+#define flush_tlb_page		local_flush_tlb_page
+#define flush_tlb_one		local_flush_tlb_one
+
+#endif /* CONFIG_MMU */
 
 #endif /* _ASM_NIOS2_TLBFLUSH_H */
