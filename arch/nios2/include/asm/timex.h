a84ce3e4fa6eab19f4e47509b18762b8194cd805
Thomas Chou <thomas@wytron.com.tw>
2011-01-18 14:01:02 +0800

    nios2: remove TIMER_1MS_FREQ in timex.h
    Follow ia64 to satisfy the compiler and ease devicetree migration.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/include/asm/timex.h
+++ arch/nios2/include/asm/timex.h
@@ -1,13 +1,8 @@
 #ifndef _ASM_NIOS2_TIMEX_H
 #define _ASM_NIOS2_TIMEX_H
 
-#include <asm/nios.h>
-
-#ifndef  TIMER_1MS_FREQ
-#error Your FPGA design does not contain a TIMER_1MS
-#endif
-
-#define CLOCK_TICK_RATE	 TIMER_1MS_FREQ /* Underlying HZ */
+/* follow ia64 */
+#define CLOCK_TICK_RATE		(HZ * 100000UL)
 
 #include <asm-generic/timex.h>
 
