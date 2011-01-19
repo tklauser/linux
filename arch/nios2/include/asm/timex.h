888fd9c8fdb795b7863364c17417f45643b959d6
Walter Goossens <waltergoossens@home.nl>
2011-01-19 17:44:02 +0100

    nios2: timex only use fake value when frequency is unknown
    Signed-off-by: Walter Goossens <waltergoossens@home.nl>

--- arch/nios2/include/asm/timex.h
+++ arch/nios2/include/asm/timex.h
@@ -1,8 +1,12 @@
 #ifndef _ASM_NIOS2_TIMEX_H
 #define _ASM_NIOS2_TIMEX_H
 
-/* follow ia64 */
+#if defined(TIMER_1MS_FREQ)
+#define CLOCK_TICK_RATE		TIMER_1MS_FREQ
+#else
+/* Supply dummy tick-rate. Real value will be read from devicetree */
 #define CLOCK_TICK_RATE		(HZ * 100000UL)
+#endif
 
 #include <asm-generic/timex.h>
 
