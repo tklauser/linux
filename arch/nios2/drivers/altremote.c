699511a84f5dbff2573446cf52222581090aaae7
Thomas Chou <thomas@wytron.com.tw>
2011-01-24 12:10:04 +0800

    nios2 altremote: rename compat to altr,remote-1.0
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/drivers/altremote.c
+++ arch/nios2/drivers/altremote.c
@@ -426,7 +426,7 @@ err_out:
 #ifdef CONFIG_OF
 static struct of_device_id altremote_match_table[] = {
 	{ .compatible = "altera,altremote", }, /* Will be removed! */
-	{ .compatible = "altr,altremote-8.1" },
+	{ .compatible = "altr,remote-1.0" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, altremote_match_table);
