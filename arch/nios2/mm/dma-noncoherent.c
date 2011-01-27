bac42fbe5313189e30e99bfac35cd92477729497
Tobias Klauser <klto@zhaw.ch>
2011-01-27 13:27:25 +0100

    nios2: Use __GFP_ZERO for dma_alloc_noncoherent
    Follow 57682827b9a5edb52e33af0be9082b51bffcd5c7
    
    This follows the x86 change off of memset() and on to an unconditional
    __GFP_ZERO for wrapping in to optimized page clearing by way of
    clear_highpage().
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/mm/dma-noncoherent.c
+++ arch/nios2/mm/dma-noncoherent.c
@@ -29,7 +29,6 @@
 #include <asm/cacheflush.h>
 #include <asm/io.h>
 
-
 /*
  * Warning on the terminology - Linux calls an uncached area coherent;
  * MIPS terminology calls memory areas with hardware maintained coherency
@@ -37,24 +36,25 @@
  */
 
 void *dma_alloc_noncoherent(struct device *dev, size_t size,
-	dma_addr_t * dma_handle, gfp_t gfp)
+			    dma_addr_t *dma_handle, gfp_t gfp)
 {
 	void *ret;
+
 	/* ignore region specifiers */
 	gfp &= ~(__GFP_DMA | __GFP_HIGHMEM);
 
+	/* optimized page clearing */
+	gfp |= __GFP_ZERO;
+
 	if (dev == NULL || (dev->coherent_dma_mask < 0xffffffff))
 		gfp |= GFP_DMA;
-	ret = (void *) __get_free_pages(gfp, get_order(size));
 
-	if (ret != NULL) {
-		memset(ret, 0, size);
+	ret = (void *) __get_free_pages(gfp, get_order(size));
+	if (ret != NULL)
 		*dma_handle = virt_to_phys(ret);
-	}
 
 	return ret;
 }
-
 EXPORT_SYMBOL(dma_alloc_noncoherent);
 
 void *dma_alloc_coherent(struct device *dev, size_t size,
