9b1dfc044f2838033ec7c5c26c05e2a2f115a794
Thomas Chou <thomas@wytron.com.tw>
2011-02-02 22:32:00 +0800

    nios2: move procfs cpuinfo code to cpuinfo.c
    Also reduce included header files in setup.c.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- arch/nios2/kernel/cpuinfo.c
+++ arch/nios2/kernel/cpuinfo.c
@@ -10,9 +10,13 @@
 
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/seq_file.h>
 #include <linux/string.h>
 #include <linux/of.h>
 #include <asm/cpuinfo.h>
+
+struct cpuinfo cpuinfo;
 
 static inline u32 fcpu(struct device_node *cpu, char *n)
 {
@@ -54,3 +58,80 @@ void __init setup_cpuinfo(void)
 	cpuinfo.tlb_num_ways = fcpu(cpu, "altr,tlb-num-ways");
 	cpuinfo.tlb_num_entries = fcpu(cpu, "altr,tlb-num-entries");
 }
+
+#ifdef CONFIG_PROC_FS
+
+/*
+ * Get CPU information for use by the procfs.
+ */
+static int show_cpuinfo(struct seq_file *m, void *v)
+{
+	int count = 0;
+	const u32 clockfreq = cpuinfo.cpu_clock_freq;
+
+	count = seq_printf(m,
+			"CPU:\t\tNios II/%s\n"
+			"MMU:\t\t%s\n"
+			"FPU:\t\tnone\n"
+			"Clocking:\t%u.%02u MHz\n"
+			"BogoMips:\t%lu.%02lu\n"
+			"Calibration:\t%lu loops\n",
+			cpuinfo.cpu_impl,
+			cpuinfo.mmu ? "present" : "none",
+			clockfreq / 1000000, (clockfreq / 100000) % 10,
+			(loops_per_jiffy * HZ) / 500000, ((loops_per_jiffy * HZ) / 5000) % 100,
+			(loops_per_jiffy * HZ));
+
+	count += seq_printf(m,
+			"HW:\n"
+			" MUL:\t\t%s\n"
+			" MULX:\t\t%s\n"
+			" DIV:\t\t%s\n",
+			cpuinfo.has_mul ? "yes" : "no",
+			cpuinfo.has_mulx ? "yes" : "no",
+			cpuinfo.has_div ? "yes" : "no");
+
+	count += seq_printf(m,
+			"Icache:\t\t%ukB, line length: %u\n",
+			cpuinfo.icache_size >> 10,
+			cpuinfo.icache_line_size);
+
+	count += seq_printf(m,
+			"Dcache:\t\t%ukB, line length: %u\n",
+			cpuinfo.dcache_size >> 10,
+			cpuinfo.dcache_line_size);
+
+	if (cpuinfo.mmu)
+		count += seq_printf(m,
+				"TLB:\t\t%u ways, %u entries\n",
+				cpuinfo.tlb_num_ways,
+				cpuinfo.tlb_num_entries);
+
+	return 0;
+}
+
+static void *cpuinfo_start(struct seq_file *m, loff_t *pos)
+{
+	unsigned long i = *pos;
+
+	return i < NR_CPUS ? (void *) (i + 1) : NULL;
+}
+
+static void *cpuinfo_next(struct seq_file *m, void *v, loff_t *pos)
+{
+	++*pos;
+	return cpuinfo_start(m, pos);
+}
+
+static void cpuinfo_stop(struct seq_file *m, void *v)
+{
+}
+
+const struct seq_operations cpuinfo_op = {
+	.start	= cpuinfo_start,
+	.next	= cpuinfo_next,
+	.stop	= cpuinfo_stop,
+	.show	= show_cpuinfo
+};
+
+#endif /* CONFIG_PROC_FS */
--- arch/nios2/kernel/setup.c
+++ arch/nios2/kernel/setup.c
@@ -13,29 +13,13 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/platform_device.h>
-#include <linux/delay.h>
-#include <linux/interrupt.h>
-#include <linux/fs.h>
-#include <linux/fb.h>
-#include <linux/module.h>
 #include <linux/mm.h>
 #include <linux/console.h>
-#include <linux/genhd.h>
-#include <linux/errno.h>
-#include <linux/string.h>
-#include <linux/major.h>
 #include <linux/bootmem.h>
 #include <linux/initrd.h>
-#include <linux/seq_file.h>
 #include <linux/of_fdt.h>
 #include <linux/blkdev.h>
 
-#include <asm/irq.h>
-#include <asm/byteorder.h>
-#include <asm/asm-offsets.h>
-#include <asm/pgtable.h>
 #include <asm/mmu_context.h>
 #include <asm/setup.h>
 #include <asm/prom.h>
@@ -90,8 +74,6 @@ EXPORT_SYMBOL(memory_end);
 static char default_command_line[] = CONFIG_CMDLINE;
 #endif
 char cmd_line[COMMAND_LINE_SIZE] = { 0, };
-
-struct cpuinfo cpuinfo;
 
 /*				   r1  r2  r3  r4  r5  r6  r7  r8  r9 r10 r11*/
 /*				   r12 r13 r14 r15 or2                      ra  fp  sp  gp es  ste  ea*/
@@ -223,80 +205,3 @@ void arch_gettod(int *year, int *month, int *date, int *hour, int *min, int *sec
 {
 	*year = *month = *date = *hour = *min = *sec = 0;
 }
-
-#ifdef CONFIG_PROC_FS
-
-/*
- * Get CPU information for use by the procfs.
- */
-static int show_cpuinfo(struct seq_file *m, void *v)
-{
-	int count = 0;
-	const u32 clockfreq = cpuinfo.cpu_clock_freq;
-
-	count = seq_printf(m,
-			"CPU:\t\tNios II/%s\n"
-			"MMU:\t\t%s\n"
-			"FPU:\t\tnone\n"
-			"Clocking:\t%u.%02u MHz\n"
-			"BogoMips:\t%lu.%02lu\n"
-			"Calibration:\t%lu loops\n",
-			cpuinfo.cpu_impl,
-			cpuinfo.mmu ? "present" : "none",
-			clockfreq / 1000000, (clockfreq / 100000) % 10,
-			(loops_per_jiffy * HZ) / 500000, ((loops_per_jiffy * HZ) / 5000) % 100,
-			(loops_per_jiffy * HZ));
-
-	count += seq_printf(m,
-			"HW:\n"
-			" MUL:\t\t%s\n"
-			" MULX:\t\t%s\n"
-			" DIV:\t\t%s\n",
-			cpuinfo.has_mul ? "yes" : "no",
-			cpuinfo.has_mulx ? "yes" : "no",
-			cpuinfo.has_div ? "yes" : "no");
-
-	count += seq_printf(m,
-			"Icache:\t\t%ukB, line length: %u\n",
-			cpuinfo.icache_size >> 10,
-			cpuinfo.icache_line_size);
-
-	count += seq_printf(m,
-			"Dcache:\t\t%ukB, line length: %u\n",
-			cpuinfo.dcache_size >> 10,
-			cpuinfo.dcache_line_size);
-
-	if (cpuinfo.mmu)
-		count += seq_printf(m,
-				"TLB:\t\t%u ways, %u entries\n",
-				cpuinfo.tlb_num_ways,
-				cpuinfo.tlb_num_entries);
-
-	return 0;
-}
-
-static void *cpuinfo_start(struct seq_file *m, loff_t *pos)
-{
-	unsigned long i = *pos;
-
-	return i < NR_CPUS ? (void *) (i + 1) : NULL;
-}
-
-static void *cpuinfo_next(struct seq_file *m, void *v, loff_t *pos)
-{
-	++*pos;
-	return cpuinfo_start(m, pos);
-}
-
-static void cpuinfo_stop(struct seq_file *m, void *v)
-{
-}
-
-const struct seq_operations cpuinfo_op = {
-	.start	= cpuinfo_start,
-	.next	= cpuinfo_next,
-	.stop	= cpuinfo_stop,
-	.show	= show_cpuinfo
-};
-
-#endif /* CONFIG_PROC_FS */
