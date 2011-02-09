b4da912ca94415747eeeffda13f7c1efa8f8307e
Thomas Chou <thomas@wytron.com.tw>
2011-02-09 14:33:27 +0800

    altera_gpio: add edge capture interrupt support
    Only one pin per port could be handled. We might need to use
    interrupt chip structure.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- drivers/gpio/altera_gpio.c
+++ drivers/gpio/altera_gpio.c
@@ -20,6 +20,7 @@
 #include <linux/of_device.h>
 #include <linux/of_platform.h>
 #include <linux/of_gpio.h>
+#include <linux/of_irq.h>
 #include <linux/io.h>
 #include <linux/gpio.h>
 #include <linux/slab.h>
@@ -29,11 +30,14 @@
 /* Register Offset Definitions */
 #define ALTERA_GPIO_DATA_OFFSET   0x0	/* Data register  */
 #define ALTERA_GPIO_DIR_OFFSET    0x4	/* I/O direction register  */
+#define ALTERA_GPIO_IRQ_MASK	  0x8
+#define ALTERA_GPIO_EDGE_CAP	  0xc
 
 struct altera_gpio_instance {
 	struct of_mm_gpio_chip mmchip;
 	u32 gpio_state;		/* GPIO state shadow register */
 	u32 gpio_dir;		/* GPIO direction shadow register */
+	int irq;		/* GPIO irq number */
 	spinlock_t gpio_lock;	/* Lock used for synchronization */
 };
 
@@ -54,7 +58,12 @@ static inline struct altera_gpio_instance *to_altera_gpio(
 static int altera_gpio_get(struct gpio_chip *gc, unsigned int gpio)
 {
 	struct of_mm_gpio_chip *mm_gc = to_of_mm_gpio_chip(gc);
+	struct altera_gpio_instance *chip = to_altera_gpio(mm_gc);
 
+	if (chip->irq >= 0) {
+		if (readl(mm_gc->regs + ALTERA_GPIO_EDGE_CAP) & (1 << gpio))
+			writel(1 << gpio, mm_gc->regs + ALTERA_GPIO_EDGE_CAP);
+	}
 	return (readl(mm_gc->regs + ALTERA_GPIO_DATA_OFFSET) >> gpio) & 1;
 }
 
@@ -157,6 +166,14 @@ static void altera_gpio_save_regs(struct of_mm_gpio_chip *mm_gc)
 	writel(chip->gpio_dir, mm_gc->regs + ALTERA_GPIO_DIR_OFFSET);
 }
 
+static int altera_gpio_to_irq(struct gpio_chip *gc, unsigned int gpio)
+{
+	struct of_mm_gpio_chip *mm_gc = to_of_mm_gpio_chip(gc);
+	struct altera_gpio_instance *chip = to_altera_gpio(mm_gc);
+
+	return chip->irq;
+}
+
 /*
  * altera_gpio_of_probe - Probe method for the GPIO device.
  * @np: pointer to device tree node
@@ -190,12 +207,16 @@ static int __devinit altera_gpio_of_probe(struct device_node *np)
 	if (tree_info && len >= sizeof(__be32))
 		chip->mmchip.gc.ngpio = be32_to_cpup(tree_info);
 
+	/* Check device node for interrupt */
+	chip->irq = of_irq_count(np) ? of_irq_to_resource(np, 0, NULL) : -ENXIO;
+
 	spin_lock_init(&chip->gpio_lock);
 
 	chip->mmchip.gc.direction_input = altera_gpio_dir_in;
 	chip->mmchip.gc.direction_output = altera_gpio_dir_out;
 	chip->mmchip.gc.get = altera_gpio_get;
 	chip->mmchip.gc.set = altera_gpio_set;
+	chip->mmchip.gc.to_irq = altera_gpio_to_irq;
 
 	chip->mmchip.save_regs = altera_gpio_save_regs;
 
@@ -207,7 +228,12 @@ static int __devinit altera_gpio_of_probe(struct device_node *np)
 		       np->full_name, status);
 		return status;
 	}
-	pr_info(DRV_NAME ": %s: registered\n", np->full_name);
+	if (chip->irq >= 0) {
+		/* clear edge and enable irq */
+		writel(UINT_MAX, chip->mmchip.regs + ALTERA_GPIO_EDGE_CAP);
+		writel(UINT_MAX, chip->mmchip.regs + ALTERA_GPIO_IRQ_MASK);
+	}
+	pr_info(DRV_NAME ": %s: registered, irq %d\n", np->full_name, chip->irq);
 	return 0;
 }
 
