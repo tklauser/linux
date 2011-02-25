a1400d5c5f6e9777cd2098a3f44fbf12672ff001
Tobias Klauser <klto@zhaw.ch>
2011-02-25 15:55:54 +0100

    nios2: Merge NOMMU support to kernel/sys_nios2.c
    The do_mmap2, old_mmap, old_select and sys_ipc functions were omitted as
    they are now handled by generic code.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/kernel/sys_nios2.c
+++ arch/nios2/kernel/sys_nios2.c
@@ -1,30 +1,11 @@
 /*
- * linux/arch/nios2nommu/kernel/sys_nios2.c
- *
+ * Copyright (C) 2011 Tobias Klauser <tklauser@distanz.ch>
  * Copyright (C) 2004 Microtronix Datacom Ltd.
  *
- * This file contains various random system calls that
- * have a non-standard calling sequence on the Linux/nios2nommu
- * platform.
- *
-  * All rights reserved.          
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
-*/
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
 
 #include <linux/errno.h>
 #include <linux/sched.h>
@@ -44,8 +25,8 @@
 #include <asm/cacheflush.h>
 
 asmlinkage long sys_mmap(unsigned long addr, unsigned long len,
-			unsigned long prot, unsigned long flags,
-			unsigned long fd, unsigned long offset)
+			 unsigned long prot, unsigned long flags,
+			 unsigned long fd, unsigned long offset)
 {
 	if (offset & ~PAGE_MASK)
 		return -EINVAL;
@@ -53,14 +34,17 @@ asmlinkage long sys_mmap(unsigned long addr, unsigned long len,
 	return sys_mmap_pgoff(addr, len, prot, flags, fd, offset >> PAGE_SHIFT);
 }
 
-/* sys_cacheflush -- flush the processor cache.  
- */
-asmlinkage int
-sys_cacheflush (unsigned long addr, int scope, int cache, unsigned long len)
+/* sys_cacheflush -- flush the processor cache. */
+asmlinkage int sys_cacheflush(unsigned long addr, int scope, int cache,
+			      unsigned long len)
 {
-   flush_dcache_range(addr, addr+len);
-   flush_icache_range(addr, addr+len);
-   return(0);
+#ifdef CONFIG_MMU
+	flush_dcache_range(addr, addr+len);
+	flush_icache_range(addr, addr+len);
+#else
+	flush_cache_all();
+#endif /* CONFIG_MMU */
+	return 0;
 }
 
 asmlinkage int sys_getpagesize(void)
@@ -72,6 +56,7 @@ asmlinkage int sys_getpagesize(void)
  * Do a system call from kernel instead of calling sys_execve so we
  * end up with proper pt_regs.
  */
+#ifdef CONFIG_MMU
 int kernel_execve(const char *filename,
 		  const char *const argv[],
 		  const char *const envp[])
@@ -87,18 +72,45 @@ int kernel_execve(const char *filename,
 
 	return __res;
 }
+#else
+int kernel_execve(const char *filename,
+		  const char *const argv[],
+		  const char *const envp[])
+{
+	register long __res __asm__ ("r2") = TRAP_ID_SYSCALL;
+	register long __sc  __asm__ ("r3") = __NR_execve;
+	register long __a   __asm__ ("r4") = (long) filename;
+	register long __b   __asm__ ("r5") = (long) argv;
+	register long __c   __asm__ ("r6") = (long) envp;
+	__asm__ __volatile__ ("trap" : "=r" (__res)
+			: "0" (__res), "r" (__sc), "r" (__a), "r" (__b), "r" (__c)
+			: "memory");
 
-/* Uncomment if you uncomment the syscall printk tracing in entry.S
- */
+	return __res;
+}
+
+#if defined(CONFIG_FB) || defined(CONFIG_FB_MODULE)
+#include <linux/fb.h>
+unsigned long get_fb_unmapped_area(struct file *filp, unsigned long orig_addr, unsigned long len, unsigned long pgoff, unsigned long flags)
+{
+
+	struct fb_info *info = filp->private_data;
+	return info->screen_base;
+}
+EXPORT_SYMBOL(get_fb_unmapped_area);
+#endif /* CONFIG_FB */
+#endif /* CONFIG_MMU */
+
+/* Uncomment if you uncomment the syscall printk tracing in entry.S */
 #if 0
-
-void print_syscall(int sc) {
-   printk("Syscall %d\n", sc);
+void print_syscall(int sc)
+{
+	printk("Syscall %d\n", sc);
 }
 
 
-void print_syscall_ret(int rv, int err) {
-   printk("  RET %d %d\n", err, rv);
+void print_syscall_ret(int rv, int err)
+{
+	printk("  RET %d %d\n", err, rv);
 }
-
 #endif
