7735035618c9e2a53897a5feee5e39ae98ed8fb7
Tobias Klauser <klto@zhaw.ch>
2011-02-25 16:19:08 +0100

    nios2: Merge NOMMU support to asm/cacheflush.h
    The whole cacheflush code can be unified even more. We do that at a
    later stage, once the NOMMU part is working properly.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/cacheflush.h
+++ arch/nios2/include/asm/cacheflush.h
@@ -1,34 +1,20 @@
 /*
+ * Copyright (C) 2003 Microtronix Datacom Ltd.
+ * Copyright (C) 2000-2002 Greg Ungerer <gerg@snapgear.com>
+ *
  * Ported from m68knommu.
  *
- * (C) Copyright 2003, Microtronix Datacom Ltd.
- * (C) Copyright 2000-2002, Greg Ungerer <gerg@snapgear.com>
- *
- * All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file "COPYING" in the main directory of this archive
+ * for more details.
  */
 
 #ifndef _ASM_NIOS2_CACHEFLUSH_H
 #define _ASM_NIOS2_CACHEFLUSH_H
 
 #include <linux/mm.h>
-#include <asm/page.h>
 
+#ifdef CONFIG_MMU
 extern void flush_cache_all(void);
 extern void flush_cache_mm(struct mm_struct *mm);
 extern void flush_cache_dup_mm(struct mm_struct *mm);
@@ -50,8 +36,38 @@ extern void copy_to_user_page(struct vm_area_struct *vma, struct page *page,
                               void *dst, void *src, int len);
 
 
-#define flush_cache_vmap(start, end) flush_dcache_range(start, end)
-#define flush_cache_vunmap(start, end) flush_dcache_range(start, end)
+#define flush_cache_vmap(start, end)		flush_dcache_range(start, end)
+#define flush_cache_vunmap(start, end)		flush_dcache_range(start, end)
+
+#else /* CONFIG_MMU */
+
+extern void cache_push(unsigned long vaddr, int len);
+extern void dcache_push(unsigned long vaddr, int len);
+extern void icache_push(unsigned long vaddr, int len);
+extern void cache_push_all(void);
+extern void cache_clear(unsigned long paddr, int len);
+
+#define flush_cache_all()			__flush_cache_all()
+#define flush_cache_mm(mm)			do { } while (0)
+#define flush_cache_range(vma, start, end)	cache_push((start), (end) - (start))
+#define flush_cache_page(vma, vmaddr)		do { } while (0)
+#define flush_dcache_range(start,end)		dcache_push((start), (end) - (start))
+#define flush_dcache_page(page)			do { } while (0)
+#define flush_icache_range(start,end)		icache_push((start), (end) - (start))
+#define flush_icache_page(vma,pg)		do { } while (0)
+#define flush_icache_user_range(vma,pg,adr,len)	do { } while (0)
+
+#define copy_to_user_page(vma, page, vaddr, dst, src, len) \
+	memcpy(dst, src, len)
+#define copy_from_user_page(vma, page, vaddr, dst, src, len) \
+	memcpy(dst, src, len)
+
+static inline void __flush_cache_all(void)
+{
+	cache_push_all();
+}
+
+#endif /* CONFIG_MMU */
 
 #define flush_dcache_mmap_lock(mapping)         do { } while(0)
 #define flush_dcache_mmap_unlock(mapping)       do { } while(0)
