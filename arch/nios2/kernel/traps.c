375f711c074115f8c20f6c14cf9acfc26bb372f4
Tobias Klauser <klto@zhaw.ch>
2011-03-14 18:36:14 +0100

    nios2: Include unaligned trap handling only on nios2mmu
    We have no exception table on NOMMU.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/kernel/traps.c
+++ arch/nios2/kernel/traps.c
@@ -99,7 +99,7 @@ asmlinkage void breakpoint_c(struct pt_regs *fp)
 	_exception(SIGTRAP, fp, TRAP_BRKPT, fp->ea);
 }
 
-#ifndef CONFIG_ALIGNMENT_TRAP
+#if defined(CONFIG_MMU) && !defined(CONFIG_ALIGNMENT_TRAP)
 /* Alignment exception handler */
 asmlinkage void handle_unaligned_c(struct pt_regs *fp)
 {
@@ -123,7 +123,7 @@ asmlinkage void handle_unaligned_c(struct pt_regs *fp)
 
 	_exception(SIGBUS, fp, BUS_ADRALN, addr);
 }
-#endif /* !CONFIG_ALIGNMENT_TRAP */
+#endif /* CONFIG_MMU && !CONFIG_ALIGNMENT_TRAP */
 
 /* Illegal instruction handler */
 asmlinkage void handle_illegal_c(struct pt_regs *fp)
