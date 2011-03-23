72c76999a0f49f4e09cda92b894ef44cdc81be06
Tobias Klauser <klto@zhaw.ch>
2011-03-23 16:17:28 +0100

    nios2: Depend on OF, add platform generic, disable board layer
    We now depend on device tree (CONFIG_OF) to build a kernel for Nios II.
    This can no longer be disabled.
    
    The old board method has been non-functional for some time now thus this
    change disables the board "layer", but keeps the board definitions
    around for reference.
    
    The device tree specific Kconfig and initialisation code has been moved
    to arch/nios2/platform to make the switch clear (also this is what
    microblaze uses).
    
    The nios.h header has been moved back to arch/nios2/include/asm. There
    are still 4 values defined therein depending on the design. They will be
    a bit harder to get from device tree, but they should definitely go away
    from nios.h in the future. For now they need to be changed manually.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/platform/platform.c
+++ arch/nios2/platform/platform.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2011 Thomas Chou
+ * Copyright (C) 2011 Walter Goossens
+ *
+ * This file is subject to the terms and conditions of the GNU General
+ * Public License. See the file COPYING in the main directory of this
+ * archive for more details.
+ */
+
 #include <linux/init.h>
 #include <linux/of_platform.h>
 
