fe0cddb201a6a41f32f1b089eee8bfb7bd039b67
Tobias Klauser <klto@zhaw.ch>
2011-04-14 10:32:45 +0200

    nios2: Clean up asm/pgtable_no.h
    Coding style, comment and header cleanup.
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/pgtable_no.h
+++ arch/nios2/include/asm/pgtable_no.h
@@ -1,51 +1,38 @@
-#ifndef _NIOS_PGTABLE_H
-#define _NIOS_PGTABLE_H
+/*
+ * Copyright (C) 2011 Tobias Klauser <tklauser@distanz.ch>
+ * Copyright (C) 2004 Microtronix Datacom Ltd
+ *
+ * Based on asm/pgtable_no.h from m68k which is:
+ *
+ * Copyright (C) 2000-2002 Greg Ungerer <gerg@snapgear.com>
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
 
-/*--------------------------------------------------------------------
- *
- * include/asm-nios2/pgtable.h
- *
- * Derived from various works, Alpha, ix86, M68K, Sparc, ...et al
- *
- * Copyright (C) 2004   Microtronix Datacom Ltd
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- *
- * Jan/20/2004		dgt	    NiosII
- *
- ---------------------------------------------------------------------*/
+#ifndef _ASM_NIOS2_PGTABLE_NO_H
+#define _ASM_NIOS2_PGTABLE_NO_H
 
 #include <asm-generic/4level-fixup.h>
 
-//vic - this bit copied from m68knommu version
 #include <asm/setup.h>
 #include <asm/io.h>
-#include <linux/sched.h>
 
 typedef pte_t *pte_addr_t;
 
-#define pgd_present(pgd)     	(1)       /* pages are always present on NO_MM */
+#define pgd_present(pgd)	(1)       /* pages are always present on NO_MM */
 #define pgd_none(pgd)		(0)
 #define pgd_bad(pgd)		(0)
 #define pgd_clear(pgdp)
-#define kern_addr_valid(addr) 	(1)
-#define	pmd_offset(a, b)	((void *)0)
+#define kern_addr_valid(addr)	(1)
+#define pmd_offset(a, b)	((void *)0)
 
 #define PAGE_NONE		__pgprot(0)    /* these mean nothing to NO_MM */
 #define PAGE_SHARED		__pgprot(0)    /* these mean nothing to NO_MM */
 #define PAGE_COPY		__pgprot(0)    /* these mean nothing to NO_MM */
 #define PAGE_READONLY		__pgprot(0)    /* these mean nothing to NO_MM */
 #define PAGE_KERNEL		__pgprot(0)    /* these mean nothing to NO_MM */
-//vic - this bit copied from m68knommu version
 
 extern void paging_init(void);
 #define swapper_pg_dir ((pgd_t *) 0)
@@ -96,4 +83,4 @@ extern unsigned long get_fb_unmapped_area(struct file *filp, unsigned long,
 					  unsigned long);
 #define HAVE_ARCH_FB_UNMAPPED_AREA
 
-#endif /* _NIOS_PGTABLE_H */
+#endif /* _ASM_NIOS2_PGTABLE_NO_H */
