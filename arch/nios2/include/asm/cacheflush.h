3ee6dddc58d139e43550f9989c20860fb7aaecd4
Yuriy Kozlov <ykozlov@ptcusa.com>
2011-07-06 10:00:20 +0200

    nios2: Reorder functions in asm/cacheflush.h
    Reorder the functions in asm/cacheflush.h to be the same as in
    asm-generic/cacheflush.h. Mark functions that are arch specific and
    should be removed so they are not used by drivers.
    
    Signed-off-by: Yuriy Kozlov <ykozlov@ptcusa.com>
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/cacheflush.h
+++ arch/nios2/include/asm/cacheflush.h
@@ -18,26 +18,28 @@
 extern void flush_cache_all(void);
 extern void flush_cache_mm(struct mm_struct *mm);
 extern void flush_cache_dup_mm(struct mm_struct *mm);
-extern void flush_cache_page(struct vm_area_struct *vma, unsigned long vmaddr, unsigned long pfn);
 extern void flush_cache_range(struct vm_area_struct *vma, unsigned long start, unsigned long end);
-extern void flush_dcache_all(void);
+extern void flush_cache_page(struct vm_area_struct *vma, unsigned long vmaddr, unsigned long pfn);
 #define ARCH_IMPLEMENTS_FLUSH_DCACHE_PAGE 1
-extern void flush_dcache_page(struct page* page);
-extern void flush_dcache_range(unsigned long start, unsigned long end);
-extern void flush_icache_all(void);
-extern void flush_icache_page(struct vm_area_struct *vma, struct page* page);
-extern void flush_icache_range(unsigned long start,unsigned long end);
-extern void copy_from_user_page(struct vm_area_struct *vma, struct page *page,
-                               unsigned long user_vaddr,
-                               void *dst, void *src, int len);
+extern void flush_dcache_page(struct page *page);
+
+extern void flush_icache_range(unsigned long start, unsigned long end);
+extern void flush_icache_page(struct vm_area_struct *vma, struct page *page);
+
+#define flush_cache_vmap(start, end)		flush_dcache_range(start, end)
+#define flush_cache_vunmap(start, end)		flush_dcache_range(start, end)
 
 extern void copy_to_user_page(struct vm_area_struct *vma, struct page *page,
                               unsigned long user_vaddr,
                               void *dst, void *src, int len);
+extern void copy_from_user_page(struct vm_area_struct *vma, struct page *page,
+                               unsigned long user_vaddr,
+                               void *dst, void *src, int len);
 
-
-#define flush_cache_vmap(start, end)		flush_dcache_range(start, end)
-#define flush_cache_vunmap(start, end)		flush_dcache_range(start, end)
+/* FIXME: Remove: Linux does not define these interfaces, they're arch specific. */
+extern void flush_dcache_all(void);
+extern void flush_dcache_range(unsigned long start, unsigned long end);
+extern void flush_icache_all(void);
 
 #else /* CONFIG_MMU */
 
