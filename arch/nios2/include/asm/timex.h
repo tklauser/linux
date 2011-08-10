a0ffcc13b897d0d8be0ed8fe9bd53a490c0cbe68
Tobias Klauser <klto@zhaw.ch>
2011-08-10 17:16:20 +0200

    nios2: Remove remaining reference to TIMER_1MS_FREQ
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- arch/nios2/include/asm/timex.h
+++ arch/nios2/include/asm/timex.h
@@ -1,12 +1,8 @@
 #ifndef _ASM_NIOS2_TIMEX_H
 #define _ASM_NIOS2_TIMEX_H
 
-#if defined(TIMER_1MS_FREQ)
-#define CLOCK_TICK_RATE		TIMER_1MS_FREQ
-#else
 /* Supply dummy tick-rate. Real value will be read from devicetree */
 #define CLOCK_TICK_RATE		(HZ * 100000UL)
-#endif
 
 #include <asm-generic/timex.h>
 
