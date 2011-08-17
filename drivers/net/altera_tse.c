e7d274aa67d55f4e4bb7e53bab9f5b7b9edbde5b
Tobias Klauser <klto@zhaw.ch>
2011-08-17 08:06:33 +0200

    altera_tse: Add missing include
    This is needed for request_irq/free_irq since 3.1-rc1
    
    Signed-off-by: Tobias Klauser <klto@zhaw.ch>

--- drivers/net/altera_tse.c
+++ drivers/net/altera_tse.c
@@ -21,6 +21,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 #include <linux/skbuff.h>
