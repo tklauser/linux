326565d0cbc139394bf329bb6fdef89b304f1695
Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
2011-10-07 09:40:31 +0200

    nios2: Add missing GPIOLIB gpio_to_irq/irq_to_gpio() stubs.
    Commit "always use gpiolib" (798f3f2) seems to have removed a bit too much.
    Both functions, gpio_to_irq() and irq_to_gpio() must exist as per
    Documentation/gpio.txt . Otherwise GPIOLIB won't compile.
    
    Signed-off-by: Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- arch/nios2/include/asm/gpio.h
+++ arch/nios2/include/asm/gpio.h
@@ -6,5 +6,11 @@
 #define gpio_get_value	__gpio_get_value
 #define gpio_set_value	__gpio_set_value
 #define gpio_cansleep	__gpio_cansleep
+#define gpio_to_irq	__gpio_to_irq
+
+static inline int irq_to_gpio(unsigned int irq)
+{
+	return -EINVAL;
+}
 
 #endif /* _ASM_NIOS2_GPIO_H */
