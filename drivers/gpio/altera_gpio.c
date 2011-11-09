171ed9f32f4abbf6ca8b7003543a649fb287fcdc
Tobias Klauser <tklauser@distanz.ch>
2011-11-09 08:33:13 +0100

    altera_gpio: Include linux/module.h
    This is needed after the recent header cleanup in mainline.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- drivers/gpio/altera_gpio.c
+++ drivers/gpio/altera_gpio.c
@@ -15,6 +15,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/errno.h>
 #include <linux/of_device.h>
