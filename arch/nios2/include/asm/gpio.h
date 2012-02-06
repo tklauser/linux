d56c975cddca5eed0f7670af7e08f2d1517be48f
Tobias Klauser <tklauser@distanz.ch>
2012-02-06 16:44:47 +0100

    nios2: Add guards and linux/errno.h include to asm/gpio.h
    Follow what other similar architectures (microblaze, openrisc) do.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- arch/nios2/include/asm/gpio.h
+++ arch/nios2/include/asm/gpio.h
@@ -1,7 +1,18 @@
+/*
+ * Copyright (C) 2012 Tobias Klauser <tklauser@distanz.ch>
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
+
 #ifndef _ASM_NIOS2_GPIO_H
 #define _ASM_NIOS2_GPIO_H
 
-#include <asm-generic/gpio.h>		/* cansleep wrappers */
+#include <linux/errno.h>
+#include <asm-generic/gpio.h>
+
+#ifdef CONFIG_GPIOLIB
 
 #define gpio_get_value	__gpio_get_value
 #define gpio_set_value	__gpio_set_value
@@ -12,5 +23,7 @@ static inline int irq_to_gpio(unsigned int irq)
 {
 	return -EINVAL;
 }
+
+#endif /* CONFIG_GPIOLIB */
 
 #endif /* _ASM_NIOS2_GPIO_H */
