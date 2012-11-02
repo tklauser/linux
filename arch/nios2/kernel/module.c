915deb01bf3cd7276f4b8653c2c5fa4c72e26895
Tobias Klauser <tklauser@distanz.ch>
2012-11-02 09:51:46 +0100

    nios2: Remove custom module_frob_arch_sections
    We can just rely on the 'weak' default hook in kernel/module.c
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- arch/nios2/kernel/module.c
+++ arch/nios2/kernel/module.c
@@ -70,15 +70,6 @@ void module_free(struct module *mod, void *module_region)
 
 #endif /* CONFIG_MMU */
 
-/* We don't need anything special. */
-int module_frob_arch_sections(Elf_Ehdr *hdr,
-			      Elf_Shdr *sechdrs,
-			      char *secstrings,
-			      struct module *mod)
-{
-	return 0;
-}
-
 int apply_relocate_add(Elf32_Shdr *sechdrs, const char *strtab,
 		       unsigned int symindex, unsigned int relsec,
 		       struct module *mod)
